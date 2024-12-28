d7601f5ec19dd8ba368dfd89da76d5ecaf9d8ef0
yash aher2004 <yashaher481@gmail.com>
2024-12-28 18:30:19 +0530

    added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai.jsx"
+
+function App() {
+
+  return (
+    //we can just return one fragment in jsx so collect all things in one fragment and return that
+    <>
+      <h1>Chai Aur React With Vite | By Yash Aher </h1>
+      <Chai/>
+    </>
+  )
+}
+
+export default App
